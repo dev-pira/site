cf9927367ad07b0262a40aa3c2a9a4bb3d151a37
Murilo Beltrame <murilobeltrame@gmail.com>
2022-09-13 22:17:29 -0300

    atualizando parte de palestrantes da pagina de detalhes do evento

--- pages/event-details.tsx
+++ pages/event-details.tsx
@@ -6,7 +6,18 @@ import { Navbar } from "../components/Navbar";
 import { Social } from "../components/Social";
 
 const EventDetail: NextPage = () => {
+
     const defaultWidth = '1345px'
+    const now = new Date()
+    const trilhas = [{
+        name: 'Tema da trilha',
+        talks: [
+            {title: 'Assunto de palestra muito interessante', speaker: 'Fulano da Silva Carvalho', speakerJob: 'Desenvolvedor na Ciclano', time: now},
+            {title: 'Assunto de palestra muito interessante', speaker: 'Fulano da Silva Carvalho', speakerJob: 'Desenvolvedor na Ciclano', time: now},
+            {title: 'Assunto de palestra muito interessante', speaker: 'Fulano da Silva Carvalho', speakerJob: 'Desenvolvedor na Ciclano', time: now},
+            {title: 'Assunto de palestra muito interessante', speaker: 'Fulano da Silva Carvalho', speakerJob: 'Desenvolvedor na Ciclano', time: now},
+        ]
+    }]
     const apoiadores = [1,2,3,4,5,6,7,8,9]
     return (
         <div>
@@ -54,14 +65,50 @@ Enim nibh ut vitae tristique. Nam vulputate lectus massa consequat tristique et 
                     {/* /PARTICIPE */}
                 </Box>
             </Box>
-            <div>Trilhas/Speakers</div>
+            <Box sx={{height: '688px', display:'flex',flexDirection: 'row', justifyContent: 'center', alignItems: 'center', background: '#212236'}}>
+                <Box sx={{width: defaultWidth, display: 'flex', flexDirection: 'column', alignItems: 'flex-start', gap: '24px'}}>
+                    <Typography variant="h3" sx={{color: 'white'}}>Conteúdo</Typography>
+                    {trilhas.map((trilha) => {
+                        return (
+                        <Box key={trilha.name} sx={{width: defaultWidth, display: 'flex', flexDirection: 'column', gap: '24px'}}>
+                            <Box sx={{display: 'flex', flexDirection: 'row', alignItems: 'center'}}>
+                                <Typography variant="h4" sx={{color: '#04FFB4', flex: 1}}>{trilha.name}</Typography>
+                                <Box sx={{flex: 3, height: 0, border: '1px dashed #04FFB4'}}></Box>
+                            </Box>
+                            <Box sx={{display: 'flex', gap: '36px'}}>
+                                {trilha.talks.map((talk) => {
+                                    const talkTime = `${talk.time.getHours().toString().padStart(2, '0')}h${talk.time.getMinutes().toString().padStart(2, '0')}`
+                                    return (
+                                        <Box key={talk.title} sx={{background: 'white', borderRadius: '10px'}}>
+                                            <Box sx={{background: 'linear-gradient(180.06deg, rgba(33, 34, 54, 0.15) 0.05%, #212236 74.97%)', color: 'white', display: 'flex', flexDirection: 'column', justifyContent: 'space-between', height: '362px', width: '254px'}}>
+                                                <Box sx={{display: 'flex', flexDirection: 'row', justifyContent: 'flex-end', padding: '16px'}}>
+                                                    <Box>{talkTime}</Box>
+                                                </Box>
+                                                <Box sx={{display: 'flex', flexDirection: 'column', gap: '8px', padding: '16px'}}>
+                                                    <Typography variant="h4" sx={{fontSize: '20px', background: 'linear-gradient(225.5deg, #04FFB4 9.38%, #57ACFF 95.68%)', backgroundClip: 'text', color: 'transparent'}}>
+                                                        {talk.title}
+                                                    </Typography>
+                                                    <Typography variant="h5" sx={{fontSize: '16px'}}>{talk.speaker}</Typography>
+                                                    <Typography>{talk.speakerJob}</Typography>
+                                                </Box>
+                                            </Box>
+                                        </Box>
+                                    )
+                                })}
+                            </Box>
+                        </Box>)
+                    })}
+                </Box>
+            </Box>
             <Box sx={{height: '442px', display:'flex',flexDirection: 'row', justifyContent: 'center', alignItems: 'center'}}>
                 <Box sx={{width: defaultWidth, display: 'flex', gap: '110px'}}>
                     <Box sx={{flex: 1, display: 'flex'}}>
-                        <Grid container>
+                        <Grid container sx={{display: 'flex', gap: '16px'}}>
                             {apoiadores.map((key) => {
                                 return (
-                                    <Box key={key} sx={{background: 'grey'}}>{`Apoiador ${key}`}</Box>
+                                    <Box key={key} sx={{background: '#f1f3f5', border: '1px solid #c0c0c0', width: '171px', height: '78px', display: 'flex', justifyContent: 'center', alignItems: 'center', color: 'rgba(179,180,211,.2)'}}>
+                                        {`Apoiador ${key}`}
+                                    </Box>
                                 )
                             })}
                         </Grid>

4e928d9199e516d404d14bed93766b0a0c0fb7ac
Murilo Beltrame <murilobeltrame@gmail.com>
2022-10-24 19:41:11 -0300

    atualizando story

--- components/Events/Events.stories.tsx
+++ components/Events/Events.stories.tsx
@@ -6,4 +6,54 @@ export default {
     component: Events
 } as ComponentMeta<typeof Events>
 
-export const Default: ComponentStory<typeof Events> = () => <Events />
+const Template: ComponentStory<typeof Events> = (args) => <Events {... args} />
+
+const events = [{
+    description: 'Turpis fusce et, nisl, bibendum viverra pretium duis nulla euismod.',
+    detailsLink: 'https://www.google.com',
+    participateLink: 'https://www.google.com',
+    title: 'Nome do evento',
+    type: 'Meetup'
+},{
+    description: 'Turpis fusce et, nisl, bibendum viverra pretium duis nulla euismod.',
+    detailsLink: 'https://www.google.com',
+    title: 'Nome do evento',
+    type: 'Meetup'
+},{
+    description: 'Turpis fusce et, nisl, bibendum viverra pretium duis nulla euismod.',
+    detailsLink: 'https://www.google.com',
+    title: 'Nome do evento',
+    type: 'Meetup'
+},{
+    description: 'Turpis fusce et, nisl, bibendum viverra pretium duis nulla euismod.',
+    detailsLink: 'https://www.google.com',
+    title: 'Nome do evento',
+    type: 'Meetup'
+},{
+    description: 'Turpis fusce et, nisl, bibendum viverra pretium duis nulla euismod.',
+    detailsLink: 'https://www.google.com',
+    participateLink: 'https://www.google.com',
+    title: 'Nome do evento',
+    type: 'Live'
+},{
+    description: 'Turpis fusce et, nisl, bibendum viverra pretium duis nulla euismod.',
+    detailsLink: 'https://www.google.com',
+    title: 'Nome do evento',
+    type: 'Live'
+},{
+    description: 'Turpis fusce et, nisl, bibendum viverra pretium duis nulla euismod.',
+    detailsLink: 'https://www.google.com',
+    title: 'Nome do evento',
+    type: 'Live'
+},{
+    description: 'Turpis fusce et, nisl, bibendum viverra pretium duis nulla euismod.',
+    detailsLink: 'https://www.google.com',
+    title: 'Nome do evento',
+    type: 'Live'
+}]
+
+export const Default = {
+    args: {
+        events
+    }
+}

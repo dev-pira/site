bf14815a6a56c76798f253474a91c7866dcfeeec
Gabriel Cesar Mello <95gabrielcesar@gmail.com>
2023-03-31 16:01:09 -0300

    feat(Navbar): ✨ initial responsive navbar behavior

--- components/Navbar/Navbar.tsx
+++ components/Navbar/Navbar.tsx
@@ -1,55 +1,138 @@
-import { AppBar, Box, Toolbar } from "@mui/material";
-import { Button } from "../Button";
+import * as React from 'react';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import IconButton from '@mui/material/IconButton';
+import Menu from '@mui/material/Menu';
+import MenuIcon from '@mui/icons-material/Menu';
+import Container from '@mui/material/Container';
+import Button from '@mui/material/Button';
 import { Image } from "../Image";
+import NavbarLink from '../NavbarLink';
 
-const Navbar: React.FC = () => {
-  const logoSize = { width: "145px", height: "60px" };
-  const items = [
-    { label: "Sobre", link: "/" },
-    { label: "Iniciativas", link: "#iniciativas" },
-    { label: "Eventos", link: "/eventos" },
-  ];
+const pages = [
+  { label: "Sobre", link: "/" },
+  { label: "Iniciativas", link: "#iniciativas" },
+  { label: "Eventos", link: "/eventos" },
+];
+const logoSize = { width: "145px", height: "60px" };
+
+function Navbar() {
+  const [anchorElNav, setAnchorElNav] = React.useState<null | HTMLElement>(null);
+
+  const handleOpenNavMenu = (event: React.MouseEvent<HTMLElement>) => {
+    setAnchorElNav(event.currentTarget);
+  };
+
+  const handleCloseNavMenu = () => {
+    setAnchorElNav(null);
+  };
+
   return (
     <AppBar
       position="static"
       color="transparent"
       sx={{
         boxShadow: "none",
-        px: "1.8rem",
+        py: {
+           xs: '10px',
+           md: '28px'
+        }
       }}
     >
-      <Box
-        sx={{
-          display: "flex",
-          flexWrap: "wrap",
-          flexDirection: "row",
-          alignItems: "center",
-          maxWidth: "100vw",
-          height: "100px",
-        }}
-      >
-        <Box sx={{ width: logoSize.width, height: logoSize.height }}>
-          <Image
-            src="/images/Logo.svg"
-            alt="DEVPIRA"
-            layout="responsive"
-            width={logoSize.width}
-            height={logoSize.height}
-          />
-        </Box>
-        <Toolbar sx={{ flexGrow: 1, justifyContent: "flex-end" }}>
-          <Box>
-            {items.map((item) => (
-              <Button variant="text" key={item.label} href={item.link}>
-                {item.label}
-              </Button>
-            ))}
+      <Container maxWidth="xl">
+        <Toolbar disableGutters>
+          {/* MOBILE */}
+          <Box sx={{
+            display: { xs: 'block', md: 'none' },
+            width: logoSize.width,
+            height: logoSize.height
+          }}>
+            <Image
+              src="/images/Logo.svg"
+              alt="DEVPIRA"
+              layout="responsive"
+              width={logoSize.width}
+              height={logoSize.height}
+            />
           </Box>
+          
+          <Box sx={{display: { xs: 'flex', md: 'none' } }}>
+            <IconButton
+              size="large"
+              aria-label="account of current user"
+              aria-controls="menu-appbar"
+              aria-haspopup="true"
+              onClick={handleOpenNavMenu}
+              color="inherit"
+            >
+              <MenuIcon />
+            </IconButton>
+
+            <Menu
+              id="menu-appbar"
+              anchorEl={anchorElNav}
+              anchorOrigin={{
+                vertical: 'bottom',
+                horizontal: 'left',
+              }}
+              keepMounted
+              transformOrigin={{
+                vertical: 'top',
+                horizontal: 'left',
+              }}
+              open={Boolean(anchorElNav)}
+              onClose={handleCloseNavMenu}
+              sx={{
+                display: { xs: 'block', md: 'none' },
+              }}
+            >
+              {pages.map((page, index) => (
+                <Button
+                  variant='text'
+                  key={index}
+                  onClick={handleCloseNavMenu}
+                  href={page.link}
+                  sx={{ my: 2, color: 'white', display: 'block' }}
+                >
+                  {page.label}
+                </Button>
+              ))}
+            </Menu>
+          </Box>
+          {/* END MOBILE */}
+
+          {/* DESKTOP */}
+          <Box sx={{
+            display: { xs: 'none', md: 'block' },
+            width: logoSize.width,
+            height: logoSize.height
+          }}>
+            <Image
+              src="/images/Logo.svg"
+              alt="DEVPIRA"
+              layout="responsive"
+              width={logoSize.width}
+              height={logoSize.height}
+            />
+          </Box>
+          <Toolbar sx={{
+            flexGrow: 1,
+            justifyContent: "flex-end",
+            display: { xs: 'none', md: 'flex' },
+          }}>
+            <Box>
+              {pages.map((page) => (
+                <NavbarLink key={page.label} page={page} onClick={handleCloseNavMenu} />
+              ))}
+            </Box>
+          </Toolbar>
+          {/* END DESKTOP */}
+
+          <Button variant="contained">Call to Action</Button>
         </Toolbar>
-        {/* <Button variant="contained">Call to Action</Button> */}
-      </Box>
+      </Container>
     </AppBar>
   );
-};
-
+}
 export default Navbar;
---
+++ components/NavbarLink/index.tsx
@@ -0,0 +1,23 @@
+import { Button, LinkProps, Link, SxProps} from "@mui/material";
+import React from "react";
+
+export interface NavbarLinkProps extends LinkProps {
+  page: { label: string, link: string },
+  
+}
+
+const NavbarLink: React.FC<NavbarLinkProps> = ({page, ...props}: NavbarLinkProps) => {
+  let sx: SxProps = {
+    color: '#6F718D',
+    mx: '16px',
+    my: '12px'
+  }
+  
+  return (
+    <Link href={page.link} underline="hover" sx={sx} {...props}>
+        {page.label}
+    </Link>
+  );
+};
+
+export default NavbarLink;

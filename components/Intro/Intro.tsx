df27023e05d0f7c9cb32962a784f5517ec5c4646
Gabriel Cesar Mello <95gabrielcesar@gmail.com>
2023-04-03 16:30:15 -0300

    fix(Intro): 🐛 initial grid settings

--- components/Intro/Intro.tsx
+++ components/Intro/Intro.tsx
@@ -1,64 +1,60 @@
-import { Box } from "@mui/material";
+import { Box, Container, Grid } from "@mui/material";
 import Image from "next/image";
 import { Button } from "../Button";
 import { Typography } from "../Typography";
 
 const Intro: React.FC = () => {
-  const defaultWidth = "1345px";
   return (
-    <Box
-      sx={{ display: "flex", flexDirection: "row", justifyContent: "center" }}
-    >
-      <Box
-        sx={{
-          display: "flex",
-          flexWrap: "wrap-reverse",
-          maxWidth: "100vw",
-          width: defaultWidth,
-          alignItems: "center",
-          justifyContent: "space-around",
-          py: "1.8rem",
-          px: "1.8rem",
-        }}
-      >
-        <Box
-          sx={{
-            display: "flex",
-            maxWidth: "100vw",
-            width: { xs: "100vw", sm: "100vw", md: "50%", lg: "50%" },
-            flexDirection: "column",
-            gap: "24px",
-          }}
-        >
-          <Typography variant={"h2"}>
-            Comunidade que
-            <Typography variant="span" color="gradient_green">
-              {" "}
-              conecta pessoas{" "}
+    <Container>
+      <Grid container>
+        <Grid item md={6}>
+          <Box
+            sx={{
+              display: "flex",
+              maxWidth: "100vw",
+              width: { xs: "100vw", sm: "100vw", md: "50%", lg: "50%" },
+              flexDirection: "column",
+              gap: "24px",
+            }}
+          >
+            <Typography variant={"h2"}>
+              Comunidade que
+              <Typography variant="span" color="gradient_green">
+                {" "}
+                conecta pessoas{" "}
+              </Typography>
+              e tecnologia
             </Typography>
-            e tecnologia
-          </Typography>
-          <Typography bigger>
-            Participe da comunidade que está transformando a vida das pessoas de
-            Piracicaba e região.
-          </Typography>
-          <Box sx={{ display: "flex", flexDirection: "row", gap: "8px" }}>
-            <Button href="#sobre">Saiba mais</Button>
-            <Button color="contrast" href="/eventos">
-              Eventos
-            </Button>
+            <Typography bigger>
+              Participe da comunidade que está transformando a vida das pessoas de
+              Piracicaba e região.
+            </Typography>
+            <Box sx={{ display: "flex", flexDirection: "row", gap: "8px" }}>
+              <Button href="#sobre">Saiba mais</Button>
+              <Button color="contrast" href="/eventos">
+                Eventos
+              </Button>
+            </Box>
           </Box>
-        </Box>
-        <Box
-          sx={{
-            display: "flex",
-            width: { xs: "90vw", sm: "90vw", md: "633px", lg: "633px" },
+        </Grid>
+        <Grid item xs={12} md={6} sx={{
+            display: 'flex',
+            justifyContent: {
+              xs: 'center',
+              md: 'end'
+            }
           }}
         >
-          <Image alt="" src="/images/Intro.svg" width="623px" height="510px" />
-        </Box>
-      </Box>
-    </Box>
+          <Box
+            sx={{
+              maxWidth: { xs: "90vw", sm: "90vw", md: "633px", lg: "633px" },
+            }}
+          >
+            <Image alt="Ilustração de uma pessoa usando óculos e fones de ouvido, sentada perante um computador" src="/images/Intro.svg" width={623} height={510} />
+          </Box>
+        </Grid>
+      </Grid>
+    </Container>
   );
 };
 

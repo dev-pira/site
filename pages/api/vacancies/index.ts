12a694d59726e7fad69dbdfdeab57ec05237d6bb
Murilo Beltrame <murilobeltrame@gmail.com>
2023-04-09 20:46:45 -0300

    feat: atualizando cabeçalhos da API

--- pages/api/vacancies/index.ts
+++ pages/api/vacancies/index.ts
@@ -3,7 +3,7 @@ import { createVacancy } from "../../../apis/cms";
 
 export default async function handler(request: NextApiRequest, response: NextApiResponse) {
     if (request.method !== 'POST') {
-        response.setHeader('Allow', ['POST'])
+        response.setHeader('Allow', ['POST', 'HEAD'])
         response.status(405)
     }
     else {

1c834cba4b198b584552a87093bedf3990202aee
Alexandre Ballestero de Paula <82414677+abpaula77@users.noreply.github.com>
2023-05-02 20:38:07 -0300

    atualização storybook quebrada

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,15 +1,11 @@
 module.exports = {
-  "stories": [
-    "../stories/**/*.stories.mdx",
-    "../**/*.stories.@(js|jsx|ts|tsx)"
-  ],
-  "addons": [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@storybook/addon-interactions"
-  ],
-  "framework": "@storybook/react",
-  "core": {
-    "builder": "@storybook/builder-webpack5"
+  "stories": ["../stories/**/*.stories.mdx", "../**/*.stories.@(js|jsx|ts|tsx)"],
+  "addons": ["@storybook/addon-links", "@storybook/addon-essentials", "@storybook/addon-interactions", "@storybook/addon-mdx-gfm"],
+  "framework": {
+    name: "@storybook/nextjs",
+    options: {}
+  },
+  docs: {
+    autodocs: true
   }
-}
+};

9aed2c295330a62ea9052eb4c0e812abecc40590
Gabriel Cesar Mello <95gabrielcesar@gmail.com>
2023-05-30 22:45:51 -0300

    fix: some fixes on Intro component

--- components/Intro/Intro.tsx
+++ components/Intro/Intro.tsx
@@ -5,34 +5,33 @@ import { Typography } from "../Typography";
 
 const Intro: React.FC = () => {
   return (
-    <Container>
-      <Grid container>
-        <Grid item xs={12} md={8}>
+    <Container id="intro">
+      <Grid
+        container
+        sx={{
+          mt: {
+            xs: '48px',
+            md: '92px'
+          },
+          mb: {
+            xs: '48px',
+            md: '148px'
+          },
+          flexDirection: {
+            xs: 'column',
+            md: 'row'
+          }
+        }}>
+        <Grid item xs={12} md={6}>
           <Box
             sx={{
               display: "flex",
-              width: { xs: "100vw", sm: "100vw", md: "50%", lg: "50%" },
-              flexDirection: {
-                xs: 'column',
-              },
+              flexDirection: 'column',
               gap: "24px",
-              mt: {
-                xs: 1,
-                md: 10
-              },
-              mb: {
-                xs: '48px',
-                md: '148px'
-              },
-              ml: {
-                md: '118px'
-              },
-              position: 'relative',
-              zIndex: 0
             }}
           >
             <Box>
-              <Typography variant={"h1"} >
+              <Typography variant={"h1"}>
                 Comunidade que
                 <Typography variant="introSpan" color="gradient_green">
                   {" "}
@@ -57,28 +56,23 @@ const Intro: React.FC = () => {
             </Box>
           </Box>
         </Grid>
-        <Grid item xs={12} md={4} sx={{
-            display: 'flex',
-            justifyContent: {
-              xs: 'center',
-              md: 'end'
-            },
-            minHeight: {
-              xs: '156px',
-              md: 'initial'
-            },
-            alignItems: 'center',
-            position: 'relative'
-          }}
-        >
+        <Grid item xs={12} md={6}>
           <Box
             sx={{
-              width: { xs: "90vw", sm: "90vw", md: "633px", lg: "633px" },
-              position: 'absolute',
-              zIndex: -1
+              display: 'flex',
+              justifyContent: 'center',
+              alignItems: 'center',
+              p: 2
             }}
           >
-            <Image alt="Ilustração de uma pessoa usando óculos e fones de ouvido, sentada perante um computador" src="/images/Intro.svg" width={623} height={510} />
+            <Box
+              sx={{
+                width: 623,
+                height: 510,
+              }}
+            >
+              <Image alt="Ilustração de uma pessoa usando óculos e fones de ouvido, sentada perante um computador" src="/images/Intro.svg" width={623} height={510} />
+            </Box>
           </Box>
         </Grid>
       </Grid>

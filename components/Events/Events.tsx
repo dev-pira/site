ee5af15518196a06fbf439478979bd765f83a5f6
Gabriel Cesar Mello <95gabrielcesar@gmail.com>
2023-06-05 20:20:30 -0300

    fix: 🐛 initial Events component fixes

--- components/Events/Events.tsx
+++ components/Events/Events.tsx
@@ -1,4 +1,4 @@
-import { Box } from "@mui/material";
+import { Box, Container, Grid } from "@mui/material";
 import Image from "next/image";
 import { EventCard } from "../EventCard";
 import { Button } from "../Button";
@@ -17,7 +17,6 @@ export interface EventsProps {
 }
 
 const Events: React.FC<EventsProps> = ({ events }: EventsProps) => {
-  const defaultWidth = "1345px";
   let limitedEvents = events;
   limitedEvents.sort((a, b) => {
     if (a.dateTime < b.dateTime) return 1;
@@ -25,102 +24,99 @@ const Events: React.FC<EventsProps> = ({ events }: EventsProps) => {
     return 0;
   });
   limitedEvents = limitedEvents.slice(0, 4);
-  const missingBoxes = [];
-  for (let i = 0; i < 4 - limitedEvents.length; i++) {
-    missingBoxes.push(i);
-  }
   return (
-    <Box
+    <Box 
       sx={{
-        display: "flex",
-        flexDirection: "row",
-        justifyContent: "center",
-        backgroundColor: "#212236;",
-        maxWidth: "100vw",
+        backgroundColor: '#212236',
       }}
     >
-      <Box
+    <Container>
+      <Grid
+        container
         sx={{
-          display: "flex",
-          flexDirection: "column",
-          maxWidth: "100vw",
-          rowGap: "2.5rem",
-          minHeight: "957px",
-          py: "1.8rem",
-          px: "1.8rem",
-          justifyContent: "space-around",
-        }}
-      >
-        {/* DESCRIPTION */}
-        <Box
-          sx={{
-            display: "flex",
-            flexWrap: "wrap",
-            maxWidth: "100vw",
-            alignItems: "center",
-            justifyContent: "space-around",
-            color: "white",
-          }}
-        >
-          <Image
-            alt=""
-            src="/images/Events.png"
-            width="346px"
-            height="276px"
-            objectFit="cover"
-          />
+          pt:{
+            xs: '36px',
+            md: '88px' 
+          },
+          pb:{
+            xs: '36px',
+            md: '48px' 
+          },
+          flexDirection: {
+            xs: 'column',
+            md: 'row'
+          }
+        }}>
+        <Grid item xs={12} md={6}>
+          <Box
+            sx={{
+              display: 'flex',
+              justifyContent: 'center',
+              alignItems: 'center',
+              p: 2
+            }}
+          >
+            <Box
+              sx={{
+                width: 346,
+                height: 276,
+              }}
+            >
+              <Image alt="Foto de uma das palestras realizadas pela comunidade" src="/images/Events.png" width={346} height={276} objectFit="cover" />
+            </Box>
+          </Box>
+        </Grid>
+        <Grid item xs={12} md={6}>
           <Box
             sx={{
               display: "flex",
-              width: "546px",
-              flexDirection: "column",
+              flexDirection: 'column',
               gap: "24px",
             }}
           >
-            <Typography variant="h3" color="contrast">
-              Eventos
-            </Typography>
-            <Typography color="contrast">
-              Os eventos sempre são organizados pelos voluntários. Contamos com
-              a ajuda de parceiros locais que oferecem espaços e recursos para a
-              realização. Nos ajudam nos eventos instituções de ensino, empresas
-              e entidades que têm a mesma vontade que o DEVPIRA de compartilhar
-              conhecimento.
-            </Typography>
             <Box>
+              <Typography variant="h3" color="contrast">
+                Eventos
+              </Typography>
+              <Typography color="contrast">
+                Os eventos sempre são organizados pelos voluntários. Contamos com
+                a ajuda de parceiros locais que oferecem espaços e recursos para a
+                realização. Nos ajudam nos eventos instituções de ensino, empresas
+                e entidades que têm a mesma vontade que o DEVPIRA de compartilhar
+                conhecimento.
+              </Typography>
+            </Box>
+            <Box sx={{
+              display: "flex",
+              flexDirection: "row",
+              gap: "8px"
+            }}>
               <Button color="contrast" href="/eventos">
                 Conhecer eventos
               </Button>
             </Box>
           </Box>
-        </Box>
-        {/* /DESCRIPTION */}
-        {/* CARDS */}
-        <Box
-          sx={{
-            display: "flex",
-            maxWidth: "100vw",
-            flexWrap: "wrap",
-            flexDirection: "row",
-            justifyContent: "center",
-            alignItems: "center",
-            gap: "30px",
-          }}
-        >
-          {limitedEvents.map((eventData, index) => {
-            return <EventCard key={index} {...eventData} color="primary" />;
-          })}
-          {missingBoxes.map((index) => {
-            return (
-              <Box key={index} sx={{ flex: 1 }}>
-                &nbsp;
-              </Box>
-            );
-          })}
-        </Box>
-        {/* /CARDS */}
-      </Box>
-    </Box>
+        </Grid>
+        <Grid item columns={12}></Grid>
+          {/* CARDS */}
+          <Box
+            sx={{
+              display: "flex",
+              maxWidth: "100vw",
+              flexWrap: "wrap",
+              flexDirection: "row",
+              alignItems: "center",
+              gap: "30px",
+            }}
+          >
+            {limitedEvents.map((eventData, index) => {
+              return <EventCard key={index} {...eventData} color="primary" />;
+            })}
+          </Box>
+          {/* /CARDS */}
+      </Grid>
+    </Container>
+  </Box>
   );
 };
 

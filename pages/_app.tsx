d4610b06a4d13bf58d8143d2c7dc598819cc6bef
Jhony Miler <jonatasmiler@gmail.com>
2023-06-25 17:39:36 -0300

    feat: :lipstick: Alteração para adicionar Favicon. Fixed #131
    Foi adicionado os arquivos dos favicons light e dark e alterado o arquivo _app.js para detectar a mudança de thema do navegador

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,12 +2,40 @@ import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import Head from "next/head";
 import { Analytics } from "@vercel/analytics/react";
+import { useEffect, useState } from "react";
 
 function MyApp({ Component, pageProps }: AppProps) {
+  const [faviconPath, setFaviconPath] = useState("/images/Favicon/light/");
+
+  useEffect(() => {
+    function handleColorSchemeChange() {
+      if (
+        window.matchMedia &&
+        window.matchMedia("(prefers-color-scheme: dark)").matches
+      ) {
+        setFaviconPath("/images/Favicon/dark/");
+      } else {
+        setFaviconPath("/images/Favicon/light/");
+      }
+    }
+
+    const colorSchemeQuery = window.matchMedia("(prefers-color-scheme: dark)");
+    colorSchemeQuery.addEventListener("change", handleColorSchemeChange);
+
+    // Verifica o tema dark quando o componente é montado
+    handleColorSchemeChange();
+
+    return () => {
+      colorSchemeQuery.removeEventListener("change", handleColorSchemeChange);
+    };
+  }, []);
   return (
     <>
       <Head>
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <link rel="apple-touch-icon" sizes="180x180" href={`${faviconPath}apple-touch-icon.png`} />
+        <link rel="icon" type="image/png" sizes="32x32" href={`${faviconPath}favicon-32x32.png`} />
+        <link rel="icon" type="image/png" sizes="16x16" href={`${faviconPath}favicon-16x16.png`} />
+        <link rel="manifest" href={`${faviconPath}site.webmanifest`} />
       </Head>
       <Component {...pageProps} />
       <Analytics />
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,9 +4,12 @@ export default function Document() {
   return (
     <Html>
       <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <link rel="preconnect" href="https://fonts.googleapis.com" /> 
+
         <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap" />
         <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Inter:300,400,500,700,800&display=swap"  />
+
       </Head>
       <body>
         <Main />

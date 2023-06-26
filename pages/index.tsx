42d7ac44f78e281f77c57d4c6e51f67f3117cd59
Gabriel Cesar Mello <95gabrielcesar@gmail.com>
2023-06-26 12:50:37 -0300

    fix: remove mock

--- pages/index.tsx
+++ pages/index.tsx
@@ -8,40 +8,8 @@ import { Navbar } from "../components/Navbar";
 import { Social } from "../components/Social";
 import Partners from "../components/Partners/Partners";
 import { fetchIndexData } from "../apis/cms";
-import { Partner } from "../models/model";
 
 const HomePage: NextPage = ({ eventsData }: InferGetServerSidePropsType<typeof getServerSideProps>) => {
-
-  const mockedPartners: Partner[] = [
-    {
-      name: 'não tenho logo 😊',
-      link: 'https://example.com',
-      category: 'Educação'
-    },
-    {
-      name: 'Pecege',
-      link: 'https://example.com',
-      logoUrl: 'https://picsum.photos/120/80',
-      category: 'Educação'
-    },
-    {
-      name: 'não tenho link 😊',
-      logoUrl: 'https://picsum.photos/90/90',
-      category: 'Educação'
-    },
-    {
-      name: 'AmBem Tech',
-      link: 'https://example.com',
-      logoUrl: 'https://picsum.photos/70/90',
-      category: 'Indústria'
-    },
-    {
-      name: 'não tenho categoria 😊',
-      link: 'https://example.com',
-      logoUrl: 'https://picsum.photos/90/200',
-    },
-  ]
-
   return (
     <div>
       <Navbar />
@@ -49,7 +17,7 @@ const HomePage: NextPage = ({ eventsData }: InferGetServerSidePropsType<typeof g
       <About />
       <Initiatives />
       <Events events={eventsData} />
-      <Partners description="A comunidade não tem fins lucrativos e conta com a energia das pessoas e a parceria de orgãos e instituições para levar formação, conhecimento e networking para todos. Seja também um agente ativo desse ecossistema sendo um apoiador. Entre em contato!" partners={mockedPartners} />
+      <Partners description="A comunidade não tem fins lucrativos e conta com a energia das pessoas e a parceria de orgãos e instituições para levar formação, conhecimento e networking para todos. Seja também um agente ativo desse ecossistema sendo um apoiador. Entre em contato!" />
       <Social />
       <Footer />
     </div>

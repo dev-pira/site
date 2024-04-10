702201eb7bee52504cf30ffde96b83a4192833d8
Murilo Beltrame <murilobeltrame@gmail.com>
2024-04-10 00:36:34 -0300

    correção

--- services/firebase.ts
+++ services/firebase.ts
@@ -3,12 +3,12 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyATGvx8UZfpFwMGY8jrUHf4zrcKjfIOTsc",
-  authDomain: "mbc-hello-next.firebaseapp.com",
-  projectId: "mbc-hello-next",
-  storageBucket: "mbc-hello-next.appspot.com",
-  messagingSenderId: "1074102537483",
-  appId: "1:1074102537483:web:ee6ba2f36ddc31ec1ae60e",
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID,
 };
 
 // Initialize Firebase

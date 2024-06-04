bddd5bb25d41fd3fb12a10d6d29c00d1298b9b39
Alexandre Ballestero de Paula <82414677+abpaula77@users.noreply.github.com>
2024-06-04 20:20:46 -0300

    atualização da pagina de contatos

--- services/contactService.ts
+++ services/contactService.ts
@@ -7,35 +7,42 @@ export function fetchMembersData(): Member[] {
       job: "Desenvolvedor Delphi @ Projeto ACBr",
       profileUrl: "https://www.linkedin.com/in/abpaula77/",
       portraitUrl:
-        "https://media.licdn.com/dms/image/D4D03AQGiCdAeggTyUQ/profile-displayphoto-shrink_800_800/0/1687351973652?e=1712188800&v=beta&t=MZMEPpTWMBOPo0QqfoENoGljwzCiBijcvhYyhgWXvYE",
+        "https://media.licdn.com/dms/image/D4D03AQGiCdAeggTyUQ/profile-displayphoto-shrink_200_200/0/1687351973652?e=1723075200&v=beta&t=SuberU-5ROBlrV5qsEsBOYnegmyo1C9KVgEfXSpvsL0",
     },
+    {
+      name: "Ana Silveira",
+      job: "Fullstack Developer @ SysMap Solutions",
+      profileUrl: "https://www.linkedin.com/in/anasilveira9787/",
+      portraitUrl:
+        "https://media.licdn.com/dms/image/D4D03AQFjaXPnGQgPxw/profile-displayphoto-shrink_200_200/0/1699282869575?e=1723075200&v=beta&t=mtmWDo1jCR_ekzPTOLqtJOoXwt1xUYa9Gg1FydWTrXs",
+    },    
     {
       name: "Fábio Baldin",
       job: "Gerente de projetos de tecnologia @ Ticket",
       profileUrl: "https://www.linkedin.com/in/fabiobaldin/",
       portraitUrl:
-        "https://media.licdn.com/dms/image/D4D03AQF6LMI0LR4A-w/profile-displayphoto-shrink_800_800/0/1698623212290?e=1712188800&v=beta&t=xsLtuAx642OzHbjABrhYdeun6H0_SAxjsM_rwrmI5c4",
+        "https://media.licdn.com/dms/image/D4D03AQF6LMI0LR4A-w/profile-displayphoto-shrink_200_200/0/1698623212290?e=1723075200&v=beta&t=xSULkpJm6guVzyY6hW1q-W5_yLPUZ8yDWRB1AQGPTPc",
     },
     {
       name: "João Lucas",
       job: "IT Engineer Coordinator @ Banco Itaú",
       profileUrl: "https://www.linkedin.com/in/jlgrisolia/",
       portraitUrl:
-        "https://media.licdn.com/dms/image/D4D03AQGEaDZ0m2bN_g/profile-displayphoto-shrink_800_800/0/1703969817756?e=1712188800&v=beta&t=zS2oKeq_6m8YnzoBmPiztPgq9IHYLOD1DMGHsgbD6eU",
+        "https://media.licdn.com/dms/image/D4D03AQGEaDZ0m2bN_g/profile-displayphoto-shrink_200_200/0/1703969817756?e=1723075200&v=beta&t=A-eyLuLvX1De4m-7b0ctFbOCyG9tREXHy5ldHMMljfo",
     },
     {
       name: "Murilo Beltrame",
       job: "Solution Architect @ Ambev Tech",
       profileUrl: "https://www.linkedin.com/in/murilobeltrame/",
       portraitUrl:
-        "https://media.licdn.com/dms/image/C4E03AQFAZlS-74Dx4g/profile-displayphoto-shrink_800_800/0/1607965784808?e=1712188800&v=beta&t=z5BkagzyU98IvaS5bmQhFHvRSF2uiG_QiqFlE8gkJAs",
+        "https://media.licdn.com/dms/image/C4E03AQFAZlS-74Dx4g/profile-displayphoto-shrink_200_200/0/1607965784808?e=1723075200&v=beta&t=Xy1uRgGYUXomKz9PeVJGqytjFP6m6F0PhynZ5PuoRO8",
     },
     {
       name: "(Lalli) Naiade Lameirão de Lima",
       job: "Flutter Senior Developer @ ioasys",
       profileUrl: "https://www.linkedin.com/in/naiadelalli/",
       portraitUrl:
-        "https://media.licdn.com/dms/image/D4D03AQES5b8R3EyRjA/profile-displayphoto-shrink_800_800/0/1688420416333?e=1712188800&v=beta&t=O1YNvYGjt1MvbqPFMcXFy2aTvhFutV5QFOzhoKpEx3k",
+        "https://media.licdn.com/dms/image/D4D03AQES5b8R3EyRjA/profile-displayphoto-shrink_200_200/0/1688420416333?e=1723075200&v=beta&t=hgo7OxgcwVbmH7m62MggUBY9ARHROqmu4wTgUFQaMfc",
     },
   ];
 }
